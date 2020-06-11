d5a81dd08c2b498ab307dc68496de71342114061
ashley <ashleykubera@gmail.com>
2020-06-11 14:06:07 -0400

    initial commit

---
+++ src/main/java/co/grandcircus/VoteAssist/VoteAssistApplication.java
@@ -0,0 +1,13 @@
+package co.grandcircus.VoteAssist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteAssistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/grandcircus/VoteAssist/VoteAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grandcircus.VoteAssist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
