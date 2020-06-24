3b29fee5e08431bcb057b46655a2a483834f8166
William <btrigona@gmail.com>
2020-06-24 16:49:44 -0400

    minor tweaks

--- src/main/java/co/grandcircus/VoteAssist/repository/ScriptRepository.java
+++ src/main/java/co/grandcircus/VoteAssist/repository/ScriptRepository.java
@@ -1,16 +1,13 @@
 package co.grandcircus.VoteAssist.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
 
 import co.grandcircus.VoteAssist.entity.Scripts;
 
 public interface ScriptRepository extends JpaRepository <Scripts, Long> {
 	
 	Scripts findByScriptName(String scriptName);
-	// Scripts deleteByScriptName(String scriptName);
 	
-	// @Query (value = "UPDATE scripts SET script_text WHERE script_name = ?;", nativeQuery = true)
-	// String updateScriptText(String scriptText);
 
 }
